263bd26c104f4cbda12d26a36f6c6668d6dc0204
Jesse Dearing <jesse.dearing@gmail.com>
2012-07-08 18:09:06 -0500

    Adds rails integration app

---
+++ spec/integration/exception_hub_integration/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ExceptionHubIntegration::Application.config.secret_token = '114eedfd01b6b9c4da401bf8c25f1b5a6dec77dc84716e8c418898f78cf5f579ce1a31602f3978a61272176dd6eb459ec5a85c9b1372aa531ed11ec7298c8928'
