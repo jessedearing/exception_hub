693dd83f67e458a6eed15f1ad597c1b1da462414
Jesse Dearing <jesse.dearing@gmail.com>
2012-07-08 20:23:48 -0500

    Dynamically filters tests based on environment vairables

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,13 @@
 require 'exception_hub'
+require 'bundler/setup'
 Bundler.require(:default, :test)
 
 RSpec.configure do |config|
+  TAGS = [:slow_test]
+
   config.treat_symbols_as_metadata_keys_with_true_values = true
   config.run_all_when_everything_filtered = true
-  config.filter_run :focus
+  TAGS.each do |tag|
+    config.filter_run_excluding tag => true unless ENV["RUN_#{tag.to_s.upcase}"]
+  end
 end
